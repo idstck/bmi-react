d302274fcc98453a8b64f7cde49339cfa55bddb2
IDStack <idstackdeveloper@gmail.com>
2022-01-16 01:13:46 +0700

    mengirim data dari child component ke parent component

--- src/components/Bmi.js
+++ src/components/Bmi.js
@@ -1,43 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import BmiForm from './BmiForm'
+import React, { useState } from 'react'
+import BmiCalculator from './BmiCalculator'
+
 
 const Bmi = () => {
-
-    const [count, setCount] = useState({
-        heightCount: '0',
-        weightCount: '0'
-    })
-
-    const { heightCount, weightCount } = count
-
-    const onChangeInput = (e) => {
-        const { name, value } = e.target
-        setCount(prevState => ({
-            ...prevState,
-            [name]: value
-        }))
-    }
-
-    const resetForm = (e) => {
-        e.preventDefault()
-        setCount({
-            heightCount: '0',
-            weightCount: '0'
-        })
-    }
-
-    const metricBmi = (height, weight) => {
-        if(height > 0 && weight > 0) {
-            const bmi = (weight / height / height) * 10000
-            console.log(bmi)
-        }
-    }
-
-    useEffect(() => {
-        // akan menghitung bmi dari tinggi dan berat melalui method
-        // metricBmi(123, 123)
-        metricBmi(heightCount, weightCount)
-    }, [heightCount, weightCount])
+    const [bmiValue, setBmiValue] = useState(0)
 
     return (
         <>
@@ -47,25 +13,9 @@ const Bmi = () => {
                         <div className="card-header bg-secondary text-white">
                             BMI Calculator
                         </div>
-                        <div className="card-body">
-                            <BmiForm 
-                                title={`Height (cm)`}
-                                type='number'
-                                name='heightCount'
-                                value={heightCount}
-                                onChange={onChangeInput}
-                                />
-                            <BmiForm 
-                                title={`Weight (kg)`}
-                                type='number'
-                                name='weightCount'
-                                value={weightCount}
-                                onChange={onChangeInput}
-                                />
-                            <button 
-                                onClick={resetForm}
-                                className="btn btn-sm btn-primary">Reset</button>
-                        </div>
+                        <span>Body Mass Index (BMI) = {bmiValue}</span>
+                        <BmiCalculator 
+                            getBmiValue={setBmiValue}/>
                         <div className="card-footer bg-primary text-white">
                             -
                         </div>
